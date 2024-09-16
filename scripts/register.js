b1beaf0564f6e783db416cb6b43b812aa8239e56
Gideon Odoom <gidodoom@gmail.com>
2024-09-16 11:37:54 +0000

    first commit

---
+++ scripts/register.js
@@ -0,0 +1,12 @@
+// alert ("JavaScript Works!");
+
+// select form by id 
+const registerForm = document.getElementById("register-form");
+// Hnadle form submit event
+registerForm.onsubmit = function (){
+    // Collect username
+    // Collect email
+    // Collect password
+    // Save user information
+    // Send a confirmation email
+}
