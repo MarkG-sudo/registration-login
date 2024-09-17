70953b9a2e5882f6a1ce6f70b4c58e95707f1a8c
Gideon Odoom <gidodoom@gmail.com>
2024-09-17 11:52:17 +0000

    adding functions and returning

--- scripts/register.js
+++ scripts/register.js
@@ -1,12 +1,37 @@
 // alert ("JavaScript Works!");
 
 // select form by id 
-const registerForm = document.getElementById("register-form");
-// Hnadle form submit event
-registerForm.onsubmit = function (){
+const registerForm = document.querySelector("#register-form");
+console.log (registerForm);
+
+// Handle form submit event
+registerForm.addEventListener("submit",  function (){
     // Collect username
     // Collect email
     // Collect password
     // Save user information
     // Send a confirmation email
+});
+
+// registerForm.onsubmit = function (){
+//     // Collect username
+//     // Collect email
+//     // Collect password
+//     // Save user information
+//     // Send a confirmation email
+// }
+
+
+// Age Variable 
+const age = 44;
+// square of age
+function squareAge (age) {
+    const squaredAge = age ** 2;
+   return squaredAge;
 }
+
+console.log(squareAge(age))
+
+
+const squaredAge = squareAge(45);
+console.log(squaredAge);
